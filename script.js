9bf10f7803fce20a37d88cd0729c4560c38b98a5
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-09-05 11:41:17 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let map = L.map('map').setView([59.8965, 30.3264], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
